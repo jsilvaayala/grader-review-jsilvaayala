9fe360318b37a131aa68b9577ff564321786d412
jsilvaayala <jsilvaayala@ucsd.edu>
2023-05-11 16:20:55 -0700

    ok

--- grade.sh
+++ grade.sh
@@ -6,11 +6,46 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
-echo 'Finished cloning'
 
+echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]]
+then 
+        echo 'ListExamples.java found'
+    else
+        echo 'ListExamples.java not found' 
+        echo 'Score 0/4'
+        exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area
+javac -cp $CPATH grading-area/*.java
+
+if [[ ! $? = 0 ]]
+then 
+    echo 'Compiler error in files'
+    echo 'Score 0/4'
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+
+FALIURES= grep -c junit-output.txt
+
+if [[ $FALIURES -eq 0 ]]
+then 
+    echo "All tests passed"
+    echo 'Score 4/4'
+    exit 1
+elif [[ $FALIURES -gt 0 ]]
+    grep Failures: junit-output.
+    exit 1
+fi
+
